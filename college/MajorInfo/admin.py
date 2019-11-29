ba3147c1b1e1d3eab44f90e13845f098352becde
Minseo-Jeong <many0922@gmail.com>
2019-11-29 11:40:32 +0900

    ubuntu_setting

--- college/MajorInfo/admin.py
+++ college/MajorInfo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from MajorInfo.models import Major_info
 
 # Register your models here.
+
+admin.site.register(Major_info)
--- college/prediction/admin.py
+++ college/prediction/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from prediction.models import Predict_list
 # Register your models here.
+
+admin.site.register(Predict_list)
