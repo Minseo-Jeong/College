6dda058f3ede476875d5a1a825808687a639ffd1
Minseo-Jeong <msny0922@gmail.com>
2019-12-02 18:33:29 +0900

    commit model change before

--- college/collegeInfo/models.py
+++ college/collegeInfo/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class College_info(models.Model):
     college_name = models.CharField(primary_key=True, max_length=50)
+    
     info = models.TextField()
     established = models.TextField()
     location = models.CharField(max_length=50)
